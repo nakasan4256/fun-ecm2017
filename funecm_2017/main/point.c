5f772c787301f29e4c60c8081e8cfafc4e23e40e
keifujunaga <b1015082@gmail.com>
2017-08-03 15:54:11 +0900

    Add files via upload

--- funecm_2017/main/point.c
+++ funecm_2017/main/point.c
@@ -49,3 +49,78 @@ void exttopro(PROJECTIVE_POINT R, const EXTENDED_POINT P)
 	mpz_set(R->Y, P->Y);
 	mpz_set(R->Z, P->Z);
 }
+
+void protomon(MONTGOMERY_POINT R, const PROJECTIVE_POINT P, const PROJECTIVE_POINT P1, const PROJECTIVE_POINT P2, const mpz_t a, const mpz_t b, const mpz_t N)
+{//projective曲線のkPの座標yをmontgomery曲線のkpの座標yに変更
+  mpz_t A,B,C,D,E,F,G,H,I,J,K,L,inv;
+  mpz_inits(A,B,C,D,E,F,G,H,I,J,K,L,inv,NULL);
+  //A = P1->X * P->X = x1*x
+  mpz_mul_mod(A,P1->X,P->X,N);
+  //B = A + 1 = x1*x+1
+  mpz_add_ui(B,A,1);
+  //C = P1->X + P->X = x1+x
+  mpz_add(C,P1->X,P->X);
+  //D = a * 2 = 2a
+  mpz_mul_ui(D,a,2);
+  //E = C + D = x1+x+2a
+  mpz_add(E,C,D);
+  //F = B * E = (x1*x+1)*(x1+x+2a)
+  mpz_mul_mod(F,B,E,N);
+  //G = F - D = (x1*x+1)*(x1+x+2a)-2a
+  mpz_sub(G,F,D);
+  //H = P1->X - P->X = (x1-x)
+  mpz_sub(H,P1->X,P->X);
+  //H = H * H = (x1-x)^2
+  mpz_mul_mod(H,H,H,N);
+  //I = H * P2->X = (x1-x)^2*x2
+  mpz_mul_mod(I,H,P2->X,N);
+  //J = G - I = (x1*x+1)*(x1+x+2a)-2a-(x1-x)^2*x2
+  mpz_sub(J,G,I);
+  //K = b * 2 = 2b
+  mpz_mul_ui(K,b,2);
+  //L = K * P->Y = 2b*y
+  mpz_mul(L,K,P->Y);
+  //R->Y = J / L = (x1*x+1)*(x1+x+2a)-2a-(x1-x)^2*x2 / 2b*y
+  mpz_invert(inv,L,N);
+  mpz_mul_mod(R->Y,J,inv,N);
+  mpz_clears(A,B,C,D,E,F,G,H,I,J,K,L,inv,NULL);
+}
+
+void exttomon(MONTGOMERY_POINT R, const EXTENDED_POINT P,const mpz_t N)
+{//Edwards曲線でのX,Y座標をmontgomery曲線でのX,Z座標に変換
+  mpz_t add,sub;
+  mpz_inits(add,sub,NULL);
+  //add = P->Y + 1 = y+1
+  mpz_add_ui(add,P->Y,1);
+  //sub = P->Y - 1 = y-1
+  mpz_sub_ui(sub,P->Y,1);
+  //sub = 1 / sub = 1/(y-1)
+  mpz_invert(sub,sub,N);
+  //R->X = add * sub = (y+1)*{1/(y-1)} = (y+1)/(y-1)
+  mpz_mul_mod(R->X,add,sub,N);
+  //R->Z = 1
+  mpz_set_ui(R->Z,1);
+  mpz_clears(add,sub,NULL);
+}
+
+void montgomery_coefficient (mpz_t A, mpz_t B,const mpz_t d, const mpz_t N)
+{//montgomery曲線の係数A,Bをedward曲線のdから算出
+  mpz_t C,D;
+  mpz_inits(C,D,NULL);
+  //C = d - 1
+  mpz_sub_ui(C,d,1);
+  //D = d + 1
+  mpz_add_ui(D,d,1);
+  //D = D * -1 = -d-1
+  mpz_mul_ui(D,D,-1);
+  //D = 1 / D = 1/(-d-1)
+  mpz_invert(D,D,N);
+  //C = C * 2 = 2*(d-1)
+  mpz_mul_ui(C,C,2);
+  //A = C * D = 2*(d-1)/(-d-1)
+  mpz_mul_mod(A,C,D,N);
+  //B = D * 4 = 4*(-d-1)
+  mpz_mul_ui(B,D,4);
+  mpz_clears(C,D,NULL);
+}
+ 
--- funecm_2017/main/point.h
+++ funecm_2017/main/point.h
@@ -27,10 +27,20 @@ typedef struct
 	mpz_t Z;
 } EXTENDED_POINT[1];
 
+typedef struct
+{
+  mpz_t X;
+  mpz_t Y;
+  mpz_t Z;
+} MONTGOMERY_POINT[1];
+
 void afftopro(PROJECTIVE_POINT R, const AFFINE_POINT P);
 void protoaff(AFFINE_POINT R, const PROJECTIVE_POINT P, const mpz_t N);
 void protoext(EXTENDED_POINT R, const PROJECTIVE_POINT P, const mpz_t N);
 void exttopro(PROJECTIVE_POINT R, const EXTENDED_POINT P);
+void protomon(MONTGOMERY_POINT R, const PROJECTIVE_POINT P, const PROJECTIVE_POINT P1, const PROJECTIVE_POINT P2, const mpz_t a, const mpz_t b, const mpz_t N);
+void exttomon(MONTGOMERY_POINT R, const EXTENDED_POINT P,const mpz_t N);
+void montgomery_coefficient (mpz_t A, mpz_t B,const mpz_t d, const mpz_t N);
 
 #define affine_point_init(P) do { mpz_init(P->x); mpz_init(P->y); } while (0)
 #define affine_point_clear(P) do { mpz_clear(P->x); mpz_clear(P->y); } while (0)
@@ -47,4 +57,8 @@ void exttopro(PROJECTIVE_POINT R, const EXTENDED_POINT P);
 #define extended_point_set(R, P) do { mpz_set(R->X, P->X); mpz_set(R->Y, P->Y); mpz_set(R->T, P->T); mpz_set(R->Z, P->Z); } while (0)
 #define extended_point_cmp(P, Q) (((mpz_cmp(P->X, Q->X) == 0) && (mpz_cmp(P->Y, Q->Y) == 0) && (mpz_cmp(P->Z, Q->Z) == 0)) ? 0 : 1)
 
+#define montgomery_point_init(P) do { mpz_init(P->X); mpz_init(P->Y); mpz_init(P->Z); } while (0)
+#define montgomery_point_clear(P) do { mpz_clear(P->X); mpz_clear(P->Y); mpz_clear(P->Z); } while (0)
+#define montgomery_point_set(R, P) do { mpz_set(R->X, P->X); mpz_set(R->Y, P->Y); mpz_set(R->Z, P->Z); } while (0)
+#define montgomery_point_cmp(P, Q) (((mpz_cmp(P->X, Q->X) == 0) && (mpz_cmp(P->Y, Q->Y) == 0) && (mpz_cmp(P->Z, Q->Z) == 0)) ? 0 : 1)
 #endif
